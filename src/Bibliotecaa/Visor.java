644caf21e79a4746ae93c6ec83ccf19fed38437d
David Mangas <David Mangas@HZ114824.H012108.NET>
2023-02-01 08:36:34 +0100

    Intinerator

--- src/Bibliotecaa/Visor.java
+++ src/Bibliotecaa/Visor.java
@@ -1,5 +1,28 @@
 package Bibliotecaa;
 
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
+
 public class Visor {
+	public static void mostrarLibros() {
+		
+		Iterator<String> libro = libro.iterator();
+		while(libro.hasNext()){
+			String elemento = libro.next();
+			JOptionPane.showInputDialog(null, elemento+" estos son todos los libros que hay");
+		
+		}
+		
+	}
+	
+	//Muestra los libros
+	public static void mostrarlibro(Libro libro) {
+	
+}
+	
+	public  void   mostrarMensaje(String mensaje) {
+		
+	}
 
 }
